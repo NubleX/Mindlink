d72876c0f3213af93a6f386da02299d4b2b2963e
NubleX <nublexer@hotmail.com>
2025-08-09 17:07:44 +0200

    i think my vscode is bugging

---
+++ target/debug/deps/mindlink-e4d98ce3cd8ed550.d
@@ -0,0 +1,7 @@
+C:\Users\nuble\projects\Mindlink\target\debug\deps\mindlink-e4d98ce3cd8ed550.d: src\main.rs src\ai.rs src\ai_memory.rs
+
+C:\Users\nuble\projects\Mindlink\target\debug\deps\libmindlink-e4d98ce3cd8ed550.rmeta: src\main.rs src\ai.rs src\ai_memory.rs
+
+src\main.rs:
+src\ai.rs:
+src\ai_memory.rs:
